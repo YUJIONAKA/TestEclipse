22406843d9c60549f79e0520c6735891177b3d9d
YUJIONAKA <y.onaka0120@gmail.com>
2025-05-23 14:20:43 +0900

    Update HelloGit.java

--- testGit/src/testGit/java/HelloGit.java
+++ testGit/src/testGit/java/HelloGit.java
@@ -3,6 +3,8 @@ package testGit.java;
 public class HelloGit {
         public static void main(String[] args) {
 		    System.out.println("Hello Github");
+		System.out.println("GoodBye Github");
+		System.out.println("GoodMorning Github");
 	}
 
 }
