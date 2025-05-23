0cc49a37733b5ea726e8a9f4da99bee747035640
YUJIONAKA <y.onaka0120@gmail.com>
2025-05-23 14:31:29 +0900

    goodbye

--- testGit/src/testGit/java/HelloGit.java
+++ testGit/src/testGit/java/HelloGit.java
@@ -3,6 +3,7 @@ package testGit.java;
 public class HelloGit {
         public static void main(String[] args) {
 		    System.out.println("Hello Github");
+		    System.out.println("GoodBye Github");
 	}
 
 }
